0a6a044c9420d9e019e27ca4d3123b14e5fbe20e
Gabriel Gene <gabrielgene97@gmail.com>
2017-01-21 16:03:47 -0300

    Remove unnecessary imports

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import {Provider} from 'react-redux'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Card from './CardExampleExpandable'
-import Counter from './Counter'
 import SelectFieldExampleSimple from './SelectFieldExampleSimple'
-import LayoutAppBar from './LayoutAppBar';
+import LayoutAppBar from './LayoutAppBar'
 import store from '../store'
 
 const App = () => (
