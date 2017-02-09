bd7ab5a2250a79bba59fe284a63efb86e4e79081
Gabriel Gene <gabrielgene97@gmail.com>
2017-02-08 22:49:23 -0300

    import styles

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import App from './Components/App'
+import './style.css'
 
 injectTapEventPlugin();
 
