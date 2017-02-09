8d3b8ce833f495a9f4beb06eca94334f7098cec9
Gabriel Gene <gabrielgene97@gmail.com>
2017-02-08 22:47:36 -0300

    New route css

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
                 test:/\.jsx?$/,
                 exclude:/node_modules/,
                 loader:"babel-loader"
+            },
+            {
+                test:/\.css$/,
+                loaders:["style", "css"]
             }
         ]
     },
