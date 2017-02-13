ec9d26abb9f7c6f04251990bd8886795a7030714
Gabriel Gene <gabrielgene97@gmail.com>
2017-02-13 14:24:08 -0300

    Insert Brunno Profile

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
-import { Card, CardText} from 'material-ui/Card';
+import { Card, CardText } from 'material-ui/Card';
 import Avatar from 'material-ui/Avatar';
 import Divider from 'material-ui/Divider';
 
 const About = () => (
   <div>
     <Card >
-        <CardText>
-          Tudo começou no inicio do semestre passado, época de arthe e todo mundo pensando em algum tipo de projeto,
+      <CardText>
+        Tudo começou no inicio do semestre passado, época de arthe e todo mundo pensando em algum tipo de projeto,
           até que meu amigo <strong> Michel Ribeiro </strong> teve uma sacada e lembrou desse problema relacionado
-          ao descobrir onde é a sua sala de aula e aquele mural chato, mas depois pensamos em outro projeto. 
-          O <strong>Cadê Minha Sala</strong> não ficou de lado, com o intuito de aprender estudamos nas ferias e 
-          desenvolvemos o app, aqui estão os principais desenvolvedores e idealizadores. 
+          ao descobrir onde é a sua sala de aula e aquele mural chato, mas depois pensamos em outro projeto.
+          O <strong>Cadê Minha Sala</strong> não ficou de lado, com o intuito de aprender estudamos nas ferias e
+          desenvolvemos o app, aqui estão os principais desenvolvedores e idealizadores.
     </CardText>
       <div className="profileWrapper">
         <div className="profile">
@@ -48,6 +48,14 @@ const About = () => (
             src="https://avatars1.githubusercontent.com/u/9686240?v=3&s=460"
             />
           <h2>Felipe Matos</h2>
+        </div>
+        <Divider />
+        <div className="profile">
+          <Avatar
+            size={150}
+            src="https://avatars3.githubusercontent.com/u/15722974?v=3&s=460"
+            />
+          <h2>Brunno Araújo</h2>
         </div>
       </div>
     </Card>
