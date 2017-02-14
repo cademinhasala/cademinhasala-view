ce6de89b000bd0322559e1ad7d0b0a83f843b17f
Gabriel Gene <gabrielgene97@gmail.com>
2017-02-14 11:06:09 -0300

    Fix Safari Bug

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import App from './Components/App'
 import './style.css'
+import 'whatwg-fetch'
 
 injectTapEventPlugin();
 
