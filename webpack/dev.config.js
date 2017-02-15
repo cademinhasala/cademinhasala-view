e186f04e6b24e93e98160a78d717e815267be611
Felipe Matos Santana <felipems@yahoo.com.br>
2017-02-15 00:37:29 -0300

    Add production build

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -3,39 +3,44 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const port = process.env.PORT || 8080
 
+function resolve(to) {
+  return path.resolve(__dirname, to)
+}
+
 module.exports = {
   entry: ['babel-polyfill', 'whatwg-fetch', './src/index'],
   devServer: {
     host: '0.0.0.0',
-    port: port,
+    port,
   },
+  devtool: 'cheap-module-eval-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
+      template: resolve('../src/index.html'),
+    }),
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'app.js'
+    path: resolve('../dist'),
+    filename: 'app.js',
   },
   module: {
     loaders: [
       {
         test: /\.html$/,
-        loader: 'html'
+        loader: 'html',
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
-        loaders: ['style', 'css']
-      }
-    ]
+        loaders: ['style', 'css'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
 };
---
+++ webpack/prod.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const port = process.env.PORT || 8080
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+function resolve(to) {
+  return path.resolve(__dirname, to)
+}
+
+module.exports = {
+  entry: ['babel-polyfill', 'whatwg-fetch', './src/index'],
+  devServer: {
+    host: '0.0.0.0',
+    port,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve('../src/index.html'),
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+      comments: false,
+      sourceMap: false,
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new ExtractTextPlugin('[contenthash].[name].css'),
+  ],
+  output: {
+    path: resolve('../dist'),
+    filename: '[hash].[name].js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'html',
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract(['css?minimize']),
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+};
