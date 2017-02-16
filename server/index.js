e23a8dd21dcacf172bd5d01496fda791b26a75b1
Felipe Matos Santana <felipems@yahoo.com.br>
2017-02-16 00:21:57 -0300

    Implement conditional get

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const app = require('./app')
-const path = require('path')
+const conditional = require('koa-conditional-get')
+const etag = require('koa-etag')
 const logger = require('koa-logger')
+const path = require('path')
 const serve = require('koa-static')
-const port = process.env.PORT || 8080
 
+const port = process.env.PORT || 8080
 const root = path.resolve(__dirname, '../dist')
 
 app.use(logger())
+
+app.use(conditional())
+app.use(etag())
+
 app.use(serve(root))
 
 app.listen(port)
