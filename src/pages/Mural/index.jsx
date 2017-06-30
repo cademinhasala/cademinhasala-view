6bba1851a903dc64c62dcbaaa60b99b6a4d0002c
Gabriel Gene <gabrielgene97@gmail.com>
2017-06-30 10:25:57 -0300

    Implement mural page

--- src/pages/Mural/index.jsx
+++ src/pages/Mural/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import AppBar from 'material-ui/AppBar'
+import SearchIcon from 'material-ui/svg-icons/action/search'
+import { brown600, white } from 'material-ui/styles/colors'
+import IconButton from 'material-ui/IconButton'
+import RaisedButton from 'material-ui/RaisedButton'
+import './style.css'
 
 const Mural = () => (
-  <h1>Mural</h1>
+  <div className="container">
+    <AppBar
+      title="Mural"
+      color={brown600}
+      iconElementRight={<IconButton><SearchIcon/></IconButton>}
+      />
+    <RaisedButton
+      label="Postar no Mural"
+      labelColor={white}
+      backgroundColor={brown600}
+      className="fixedButton" />
+  </div>
 )
 
 export default Mural
