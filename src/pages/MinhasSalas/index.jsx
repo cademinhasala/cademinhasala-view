a0145d5c0994c471d17b73efd1c394de3c31377a
Gabriel Gene <gabrielgene97@gmail.com>
2017-06-30 10:26:12 -0300

    Implement minhas salas page

--- src/pages/MinhasSalas/index.jsx
+++ src/pages/MinhasSalas/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import AppBar from 'material-ui/AppBar'
+import SearchIcon from 'material-ui/svg-icons/action/search'
+import IconButton from 'material-ui/IconButton'
+import { brown600 } from 'material-ui/styles/colors'
 
 const MinhasSalas = () => (
-  <h1>MinhasSalas</h1>
+  <div className="container">
+    <AppBar
+      title="Minhas Salas"
+      iconElementRight={<IconButton><SearchIcon/></IconButton>}
+      color={brown600}
+      />
+  </div>
 )
 
 export default MinhasSalas
