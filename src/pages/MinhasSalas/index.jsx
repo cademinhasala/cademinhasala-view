55a93aa053e4adb67349b7fb929ec55cad3446c7
Gabriel Gene <gabrielgene97@gmail.com>
2017-06-30 18:34:03 -0300

    Implement minhas salas page

--- src/pages/MinhasSalas/index.jsx
+++ src/pages/MinhasSalas/index.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import AppBar from 'material-ui/AppBar'
 import SearchIcon from 'material-ui/svg-icons/action/search'
+import { green800, white } from 'material-ui/styles/colors'
 import IconButton from 'material-ui/IconButton'
-import { brown600 } from 'material-ui/styles/colors'
+import RaisedButton from 'material-ui/RaisedButton'
+import MenuItem from 'material-ui/MenuItem';
+import Drawer from 'material-ui/Drawer';
 
-const MinhasSalas = () => (
-  <div className="container">
-    <AppBar
-      title="Minhas Salas"
-      iconElementRight={<IconButton><SearchIcon/></IconButton>}
-      color={brown600}
-      />
-  </div>
-)
+export default class MinhasSalas extends React.Component {
 
-export default MinhasSalas
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  };
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          title="Minhas Salas"
+          style={{backgroundColor: green800}}
+          iconElementRight={<IconButton><SearchIcon/></IconButton>}
+          onLeftIconButtonTouchTap={this.handleToggle}
+        />
+        <Drawer
+          docked={false}
+          width={250}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem onTouchTap={this.handleClose} href='/#/' >Salas</MenuItem>
+          <MenuItem onTouchTap={this.handleClose} href='/#/minhas-salas'>Minhas Salas</MenuItem>
+          <MenuItem onTouchTap={this.handleClose} href='/#/mural'>Mural</MenuItem>
+          <MenuItem onTouchTap={this.handleClose} href=''>Sobre</MenuItem>
+        </Drawer>
+      </div>
+    );
+  }
+}
+
