4cef90637d2469559da9ffe9be7f8358e7f4c964
Gabriel Gene <gabrielgene97@gmail.com>
2017-06-30 10:26:40 -0300

    Add theme provider in react router

--- src/router.jsx
+++ src/router.jsx
@@ -3,17 +3,34 @@ import ReactDOM from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import Salas from './pages/Salas'
 import MinhasSalas from './pages/MinhasSalas'
+import { blueGrey500, deepOrangeA400, blueGrey700 } from 'material-ui/styles/colors'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 import Mural from './pages/Mural'
 import App from './components/App'
 
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: blueGrey500,
+    primary2Color: blueGrey700,
+    accent1Color: deepOrangeA400,
+  },
+}, {
+  userAgent: global.navigator
+    ? global.navigator.userAgent
+    : 'all',
+})
+
 const MyRouter = () => (
-  <Router history={hashHistory}>
-      <Route path="/" component={Salas} />
-      <Route path="/minhas-salas" component={MinhasSalas} />
-      <Route path="/mural" component={Mural} />
-      <Route path="/app" component={App} />
-  </Router>
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <Router history={hashHistory}>
+        <Route path="/" component={Salas} />
+        <Route path="/minhas-salas" component={MinhasSalas} />
+        <Route path="/mural" component={Mural} />
+    </Router>
+  </MuiThemeProvider>
 )
 
 export default MyRouter
