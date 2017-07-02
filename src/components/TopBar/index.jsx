5b37444a36ecb974d5d00e9a8e61a5f384b30c3b
Gabriel Gene <gabrielgene97@gmail.com>
2017-07-01 23:29:02 -0300

    Change top bar

--- src/components/TopBar/index.jsx
+++ src/components/TopBar/index.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import AppBar from 'material-ui/AppBar'
 import SearchIcon from 'material-ui/svg-icons/action/search'
 import Class from 'material-ui/svg-icons/action/class'
+import Home from 'material-ui/svg-icons/action/home'
+import Info from 'material-ui/svg-icons/action/info-outline'
 import Wallpaper from 'material-ui/svg-icons/device/wallpaper'
 import { brown600, white } from 'material-ui/styles/colors'
 import IconButton from 'material-ui/IconButton'
 import MenuItem from 'material-ui/MenuItem';
 import Menu from 'material-ui/Menu';
+import Divider from 'material-ui/Divider';
 import Drawer from 'material-ui/Drawer';
+import { Card, CardMedia} from 'material-ui/Card'
 
 export default class TopBar extends React.Component {
 
@@ -36,10 +40,17 @@ export default class TopBar extends React.Component {
           onRequestChange={(open) => this.setState({open})}
         >
           <Menu>
-          <MenuItem rightIcon={ <Wallpaper /> } leftIcon={ <Class /> } onTouchTap={this.handleClose} href='/#/' primaryText="Salas" />
-          <MenuItem onTouchTap={this.handleClose} href='/#/minhas-salas'>Minhas Salas</MenuItem>
+          <Card>
+            <img src="../../img/logo.jpg" alt="" />
+          </Card>
+          <MenuItem leftIcon={ <Home /> } onTouchTap={this.handleClose} href='/#/' primaryText="Salas" />
+          <Divider />
+          <MenuItem leftIcon={ <Class /> } onTouchTap={this.handleClose} href='/#/minhas-salas'>Minhas Salas</MenuItem>
+          <Divider />
           <MenuItem leftIcon={<Wallpaper />} onTouchTap={this.handleClose} href='/#/mural'>Mural</MenuItem>
-          <MenuItem onTouchTap={this.handleClose} href='/#/sobre'>Sobre</MenuItem>
+          <Divider />
+          <MenuItem leftIcon={ <Info /> } onTouchTap={this.handleClose} href='/#/sobre'>Sobre</MenuItem>
+          <Divider />
           </Menu>
         </Drawer>
       </div>
